c2b5339392ff59c0ad21ab4090db7837755faa2e
Mohamed Amine Mrs <45700625+MedAaM@users.noreply.github.com>
2024-10-26 04:25:03 +0100

    Update main.ts
    changes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+    // Enable CORS with specific settings
+  app.enableCors({
+    origin: true, 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
